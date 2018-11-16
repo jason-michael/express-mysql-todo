08ef2c6e5a443695e3bc69614e04e4268cac4f4e
Jason Michael <michael.jason@outlook.com>
2018-11-16 17:05:50 -0600

    improved ux

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,147 +1,220 @@
-// Initialization
+//============================
+// INITIALIZATION
+//============================
+
 getTodos();
 
-//======================================
-// FUNCTIONS
-//======================================
+
+//============================
+// FETCHING & RENDERING
+//============================
+
+function getTodos() {
+    getCompletedTodos();
+    getIncompleteTodos();
+}
+
+function getIncompleteTodos() {
+    const incompleteList = $('#incompletes');
+    let incompleteTodos = [];
+
+    $.get('/api/incomplete', data => {
+            incompleteTodos = data;
+        })
+        .done(() => {
+            incompleteList.empty();
+
+            if (!incompleteTodos.length) {
+                incompleteList.append('<p>Nothing to do yet.</p>');
+            }
+
+            $.each(incompleteTodos, (index, todo) => {
+                incompleteList.append(createTodoElem(todo));
+            });
+        })
+        .catch(err => {
+            alert(err.statusText);
+        });
+}
+
+function getCompletedTodos() {
+    const completeList = $('#completes');
+    let completedTodos = [];
+
+    $.get('/api/complete', data => {
+            completedTodos = data;
+        })
+        .done(() => {
+            completeList.empty();
+
+            if (!completedTodos.length) {
+                completeList.append('<p>Nothing completed yet.</p>');
+            }
+
+            $.each(completedTodos, (index, todo) => {
+                completeList.append(createTodoElem(todo));
+            });
+        })
+        .catch(err => {
+            alert(err.statusText);
+        });
+}
+
 function createTodoElem(todo) {
     let iconClass = (todo.done) ? 'fa-check-square' : 'fa-square';
     return $(`
-        <li class="todoItem field has-addons" todoid=${todo.id} todotask="${todo.task}" tododone=${todo.done}>
-            <div class="control">
-                <button class="button toggleBtn"}>
-                    <i class="far ${iconClass}"></i>
-                </button>
-            </div>
-            <div class="control is-expanded taskControl">
-                <button class="taskBtn button is-fullwidth">${todo.task}</button>
-                <input class="editTask" type="text" value="${todo.task}">
-            </div>
-            <div class="control">
-                <button class="button deleteBtn">
-                    <i class="fas fa-times"></i>
-                </button>
-            </div>
-        </li>
-    `);
+    <li class="todoItem field has-addons" todoid=${todo.id} todotask="${todo.task}" tododone=${todo.done}>
+        <div class="control">
+            <button class="button toggleBtn"}>
+                <i class="far ${iconClass}"></i>
+            </button>
+        </div>
+        <div class="control is-expanded taskControl">
+            <button class="taskBtn button is-fullwidth">${todo.task}</button>
+            <input class="editTask" type="text" value="${todo.task}">
+        </div>
+        <div class="control">
+            <button class="button deleteBtn">
+                <i class="fas fa-times"></i>
+            </button>
+        </div>
+    </li>`);
 }
 
-// TODO: refactor getTodos()
-function getTodos() {
-    let allTodos = [];
-    let completedTodos = [];
-    let incompleteTodos = [];
+//===========================
+// EVENT HANDLERS
+//===========================
 
-    $.get('/api/all', data => allTodos = data)
-        .done(() => {
-            // Reset input and lists.
-            $('#newTodoInput').val('');
-            $('#incompletes').empty();
-            $('#completes').empty();
-
-            // Sort all todos into complete/incomplete.
-            // This is so we can add placeholder text if a list is empty (below).
-            $.each(allTodos, (index, todo) => {
-                if (todo.done) {
-                    completedTodos.push(todo);
-                    $('#completes').append(createTodoElem(todo));
-                } else {
-                    incompleteTodos.push(todo);
-                    $('#incompletes').append(createTodoElem(todo));
-                }
-            });
-
-            // Add placeholder text on empty list
-            if (!incompleteTodos.length) {
-                $('#incompletes').append('<p>Nothing to do yet.</p>');
-            }
-
-            if (!completedTodos.length) {
-                $('#completes').append('<p>Nothing completed yet.</p>');
-            }
-        })
+function addNewTodo(todo, callback) {
+    $.post('/api/add', todo)
+        .done(callback())
         .catch(err => alert(err.statusText));
 }
 
-//======================================
-// EVENT LISTENERS
-//======================================
-// TODO: refactor listeners with handler funcs
+function deleteTodo(id, callback) {
+    $.ajax({
+            type: 'DELETE',
+            url: "/api/delete/" + id,
+            data: id,
+        })
+        .done(callback())
+        .catch(err => alert(err.statusText));
+}
 
-// Add new todo
+function updateTodo(todo, callback) {
+    $.ajax({
+            type: 'PUT',
+            url: '/api/update',
+            data: todo
+        })
+        .done(callback())
+        .catch(err => alert(err.statusText));
+}
+
+//============================
+// EVENT LISTENERS
+//============================
+
+/**
+ * Add todo (on click)
+ */
 $('#addTodoBtn').on('click', () => {
     const newTodo = {
         task: $('#newTodoInput').val(),
         done: 0
     }
-
-    $.post('/api/add', newTodo)
-        .done(() => getTodos())
-        .catch(err => alert(err.statusText));
+    addNewTodo(newTodo, getTodos);
+    $('#newTodoInput').val('');
 });
 
+/**
+ * Add todo (on enter)
+ */
+$('#newTodoInput').on('keyup', function (e) {
+    if (e.keyCode !== 13) return;
 
-// Toggle todo
+    const newTodo = {
+        task: $('#newTodoInput').val(),
+        done: 0
+    }
+    addNewTodo(newTodo, getTodos);
+    $('#newTodoInput').val('');
+});
+
+/**
+ * Toggle todo
+ */
 $(document).on('click', '.toggleBtn', function () {
     const parent = $(this).parent().parent();
-
-    $.ajax({
-            type: 'PUT',
-            url: '/api/update',
-            data: {
-                id: $(parent).attr('todoid'),
-                task: $(parent).attr('todoname'),
-                done: ($(parent).attr('tododone') == 1) ? 0 : 1, // Toggle 0 and 1
-            },
-        })
-        .done(() => getTodos())
-        .catch(err => alert(err.statusText));
+    const toggledTodo = {
+        id: $(parent).attr('todoid'),
+        task: $(parent).attr('todoname'),
+        done: ($(parent).attr('tododone') == 1) ? 0 : 1, // Toggle 0 and 1
+    }
+    updateTodo(toggledTodo, getTodos);
 });
 
-
-// Delete todo
+/**
+ * Delete todo
+ */
 $(document).on('click', '.deleteBtn', function () {
     const parent = $(this).parent().parent();
     const idToDelete = $(parent).attr('todoId');
 
     if (!idToDelete) return;
 
-    $.ajax({
-            type: 'DELETE',
-            url: "/api/delete/" + idToDelete,
-            data: idToDelete,
-        })
-        .done(() => getTodos())
-        .catch(err => alert(err.statusText));
+    deleteTodo(idToDelete, getTodos);
 });
 
-
-// Show edit box
+/**
+ * Edit todo: show edit input
+ */
 $(document).on('click', '.taskBtn', function () {
     const taskInput = $(this).next();
     $(taskInput).show().focus();
     $(this).hide();
 });
 
-
-// Hide edit box, update task
+/**
+ * Edit todo: (on blur) update todo and hide edit input
+ */
 $(document).on('blur', '.editTask', function () {
     $(this).prev().show(); // Task button
-    $(this).hide();
+    $(this).hide(); // Input
 
     const parent = $(this).parent().parent();
     const updatedTask = $(this).val().trim();
     const originalTask = $(parent).attr('todotask');
-    const done = $(parent).attr('tododone');
-    const id = parseInt($(parent).attr('todoid'));
+    const updatedTodo = {
+        id: parseInt($(parent).attr('todoid')),
+        task: updatedTask,
+        done: $(parent).attr('tododone')
+    }
 
     if (updatedTask === originalTask) return;
 
-    $.ajax({
-            type: 'PUT',
-            url: '/api/update',
-            data: { id, task: updatedTask, done }
-        })
-        .done(() => getTodos())
-        .catch(err => alert(err.statusText));
+    updateTodo(updatedTodo, getTodos);
+});
+
+/**
+ * Edit todo: (on enter) update todo and hide edit input
+ */
+$(document).on('keyup', '.editTask', function (e) {
+    if (e.keyCode !== 13) return;
+
+    $(this).prev().show(); // Task button
+    $(this).hide(); // Input
+
+    const parent = $(this).parent().parent();
+    const updatedTask = $(this).val().trim();
+    const originalTask = $(parent).attr('todotask');
+    const updatedTodo = {
+        id: parseInt($(parent).attr('todoid')),
+        task: updatedTask,
+        done: $(parent).attr('tododone')
+    }
+
+    if (updatedTask === originalTask) return;
+
+    updateTodo(updatedTodo, getTodos);
 });
